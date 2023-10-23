f4d2781bd2ba2f2814bd314b4d7be9375b639db4
Lin Zeshi <linzeshi@foxmail.com>
2023-10-23 21:53:10 +0800

    update

--- include/IKFoM_toolkit/esekfom/util.hpp
+++ include/IKFoM_toolkit/esekfom/util.hpp
@@ -36,35 +36,41 @@
 #define __MEKFOM_UTIL_HPP__
 
 #include <Eigen/Core>
-#include "../mtk/src/mtkmath.hpp"
-namespace esekfom {
 
+#include "../mtk/src/mtkmath.hpp"
+namespace esekfom
+{
 template <typename T1, typename T2>
-class is_same {
-   public:
-    operator bool() { return false; }
+class is_same
+{
+public:
+  operator bool() { return false; }
 };
 template <typename T1>
-class is_same<T1, T1> {
-   public:
-    operator bool() { return true; }
+class is_same<T1, T1>
+{
+public:
+  operator bool() { return true; }
 };
 
 template <typename T>
-class is_double {
-   public:
-    operator bool() { return false; }
+class is_double
+{
+public:
+  operator bool() { return false; }
 };
 
 template <>
-class is_double<double> {
-   public:
-    operator bool() { return true; }
+class is_double<double>
+{
+public:
+  operator bool() { return true; }
 };
 
 template <typename T>
-static T id(const T &x) {
-    return x;
+static T id( const T &x )
+{
+  return x;
 }
 
 }  // namespace esekfom
--- src/options.cc
+++ src/options.cc
@@ -4,10 +4,10 @@
 
 #include "options.h"
 
-namespace faster_lio::options {
-
-int NUM_MAX_ITERATIONS = 0;
+namespace faster_lio::options
+{
+int   NUM_MAX_ITERATIONS   = 0;
 float ESTI_PLANE_THRESHOLD = 0.1;
-bool FLAG_EXIT = false;
+bool  FLAG_EXIT            = false;
 
-}  // namespace faster_lio
+}  // namespace faster_lio::options
